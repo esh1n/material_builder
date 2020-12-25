c1cf203dd1e091be816fd4767d211ac911b29e80
esh1n <eshinsergey@gmail.com>
2020-12-25 17:39:25 +0300

    fixed app name

--- app/build/generated/source/buildConfig/debug/io/material/materialthemebuilder/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/io/material/materialthemebuilder/BuildConfig.java
@@ -5,7 +5,7 @@ package io.material.materialthemebuilder;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "io.material.materialthemebuilder";
+  public static final String APPLICATION_ID = "com.esh1n.lab.themebuilder";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
